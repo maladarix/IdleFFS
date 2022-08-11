53759d62158f456b89552579babd9946a4d630ed
maladarix <maladarix@gmail.com>
2022-08-11 05:07:48 -0400

    Update dieux bilou.js

--- src/farm/dieux bilou.js
+++ src/farm/dieux bilou.js
@@ -3,7 +3,7 @@ class bilou {
   constructor() {
     this.data = {
       profit: 8300000000000,
-      cost: "54B40B1F852BDA00000",
+      cost: "39992648A23C8A00000",
     }
     this.name = "👼 Dieu Bilou"
     this.img = "https://i.imgur.com/9FO5eG5.jpg"
